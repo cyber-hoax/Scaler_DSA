1b778c2fd60076e1a01538485641b8587b0cbb9d
cyberhoax <raunaktamang@gmail.com>
2022-01-02 13:32:09 +0530

    update

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -6,9 +6,11 @@
 //
 
 #include <iostream>
-
+using namespace  std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int a =3 ;
+    int b =12;
+    int c = a^b ;
+    return c;
 }
