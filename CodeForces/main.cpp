b8c2605eb2708841a700fa0879ced87d24e11bfb
cyberhoax <raunaktamang@gmail.com>
2022-01-19 13:50:40 +0530

    update

---
+++ CodeForces/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodeForces
+//
+//  Created by CYBERHOAX on 19/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
