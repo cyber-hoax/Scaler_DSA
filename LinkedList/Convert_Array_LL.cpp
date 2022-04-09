ca4cb3fce52465c5f300dd837ce72c22060cf725
cyberhoax <raunaktamang@gmail.com>
2022-04-09 13:32:21 +0530

    update

---
+++ LinkedList/Convert_Array_LL.cpp
@@ -0,0 +1,18 @@
+//
+//  Convert_Array_LL.cpp
+//  LinkedList
+//
+//  Created by CYBERHOAX on 08/04/22.
+//
+// convert an array into Linked List
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std ;
+
+
+    
+int main(){
+        
+}
---
+++ LinkedList/ReturnHead.cpp
@@ -0,0 +1,25 @@
+//
+//  ReturnHead.cpp
+//  LinkedList
+//
+//  Created by CYBERHOAX on 08/04/22.
+//
+/*
+ create a linked list from a given array of size(n) & return head
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std ;
+
+
+
+
+int main (){
+    int A[] = {1,5,6,2,3,4} ;
+    int n = size(A)/2+1;
+    cout << n << endl;
+    cout << A[n];
+    
+}
