9aebafb6580572ce94553db120fa6c3148da43d0
cyberhoax <raunaktamang@gmail.com>
2022-05-18 13:12:48 +0530

    update

--- BitManipulation/GrayCode.cpp
+++ BitManipulation/GrayCode.cpp
@@ -6,3 +6,45 @@
 //
 
 #include <stdio.h>
+#include <bitset>
+#include <vector>
+#include <iostream>
+
+using namespace std ;
+
+
+void utils(bitset<32>&bits, vector<int> ans  ,int A){
+    if(A== 0){
+        cout << bits.to_ulong()<<endl;
+        ans.push_back(bits.to_ulong());
+        
+    }
+    else{
+        utils(bits, ans, A-1);
+        bits.flip(A-1);
+        utils(bits,ans, A-1);
+    }
+}
+
+
+
+int main(){
+    int A = 3 ;
+    vector<int> ans;
+    bitset<32> bits;
+//    utils(bits, ans , A);
+    
+    vector<int> anss(1<<A);
+    for (int i=0; i<(1<<A); i++){
+                cout << i<<A <<endl ;
+            
+                anss[i] = i^(i>>1);
+            
+        cout << anss[i] << endl ;
+    }
+            return 0;
+    
+//    return 0 ;
+}
+
+
